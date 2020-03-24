e3cc811d2f86dd596f462393b130c9931e3ae852
syedosamamaruf <syedosamamaruf@gmail.com>
2020-03-24 19:52:47 +0500

    exposed view port scroll event

--- VirtualList.svelte
+++ VirtualList.svelte
@@ -1,5 +1,7 @@
 <script>
-	import { onMount, tick } from 'svelte';
+	import { onMount, tick, createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
 
 	// props
 	export let items;
@@ -64,7 +66,7 @@
 
 	}
 
-	async function handle_scroll() {
+	async function handle_scroll(event) {
 		const { scrollTop } = viewport;
 
 		const old_start = start;
@@ -121,6 +123,8 @@
 			const d = actual_height - expected_height;
 			viewport.scrollTo(0, scrollTop + d);
 		}
+
+		dispatch('scroll', event);
 
 		// TODO if we overestimated the space these
 		// rows would occupy we may need to add some
